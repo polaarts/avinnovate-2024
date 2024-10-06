6d67b3b7539bdbbfe71a55f8732b0f2c9999a236
Samuel Angulo <87445990+polaarts@users.noreply.github.com>
2024-10-06 10:06:54 -0300

    Delete servicios/lib directory

--- servicios/lib/models.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ArtistSchema = new mongoose.Schema({
-  name: String,
-  genres: [String],
-  userId: String,  // ID único del usuario si deseas asociarlo a un usuario específico
-});
-
-export const Artist = mongoose.models.Artist || mongoose.model('Artist', ArtistSchema);
