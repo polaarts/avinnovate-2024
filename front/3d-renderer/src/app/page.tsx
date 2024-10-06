559d39a12c7f9e80d12f0ae7ebcfe53fd13fc798
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2024-10-06 10:11:39 -0300

    dsjd

--- front/3d-renderer/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import TheaterComponent from "../components/theater-seating";
-import ConcertTicketsComponent from "../components/concert-tickets";
-
-export default function Home() {
-  return (
-    <ConcertTicketsComponent />
-  );
-}
